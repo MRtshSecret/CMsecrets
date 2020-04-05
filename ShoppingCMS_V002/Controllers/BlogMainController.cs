105540d02bd3ffdadc5c83cfe4893eea37d8c8ee
MRMousaviniya <MRMousaviniya@DESKTOP-DKSE6H2>
2020-04-05 08:00:28 -0700

    postPageAdded

---
+++ ShoppingCMS_V002/Controllers/BlogMainController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingCMS_V002.Controllers
+{
+    public class BlogMainController : Controller
+    {
+        // GET: BlogMain
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NewBlogPost()
+        {
+            //ControllerContext.ParentActionViewContext.ViewBag.PageTitle = "";
+            return View();
+        }
+
+    }
+}

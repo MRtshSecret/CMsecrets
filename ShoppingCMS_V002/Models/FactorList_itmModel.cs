420a66a904d0dc815e9ebc8e5f210f8aede5a45d
user <user@DESKTOP-BERDRJR>
2020-04-12 23:15:25 +0430

    24.1.99

---
+++ ShoppingCMS_V002/Models/FactorList_itmModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingCMS_V002.Models
+{
+    public class FactorList_itmModel
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public int ItmNumbers { get; set; }
+        public long totality { get; set; }
+        public long deposit { get; set; }
+        public string priceQuantity { get; set; }
+        public string Date { get; set; }
+        public string status { get; set; }
+    }
+}
---
+++ ShoppingCMS_V002/Models/FactorModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingCMS_V002.Models
+{
+    public class FactorModel
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public int ItmNumbers { get; set; }
+        public long totality { get; set; }
+        public long deposit { get; set; }
+        public string priceQuantity { get; set; }
+        public string CustomerAddress { get; set; }
+        public string CustomerPhoneNumber { get; set; }
+        public int MyProperty { get; set; }
+        public string Date { get; set; }
+        public string status { get; set; }
+
+    }
+}

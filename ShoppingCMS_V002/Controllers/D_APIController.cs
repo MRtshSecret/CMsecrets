9c63f6fa30d755944525d6d563b883c1f5f3d3b5
user <user@DESKTOP-BERDRJR>
2020-04-25 04:13:10 +0430

    6.2.99

---
+++ ShoppingCMS_V002/Controllers/D_APIController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingCMS_V002.Controllers
+{
+    public class D_APIController : Controller
+    {
+        // GET: D_API
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ShoppingCMS_V002/Models/D_APIModels/MiniProductModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingCMS_V002.Models.D_APIModels
+{
+    public class MiniProductModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string PicPath { get; set; }
+        public string Discription { get; set; }
+        public string Price { get; set; }
+        public string OffPrice { get; set; }
+    }
+}
